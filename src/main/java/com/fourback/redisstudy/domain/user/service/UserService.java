7506bd8c72729420886f0a48bd854be6510d274c
choi <rosoa0475@gmail.com>
2024-11-15 18:11:30 +0900

    [RENAME] 공통 redis repository 이름 변경

--- src/main/java/com/fourback/redisstudy/domain/user/service/UserService.java
+++ src/main/java/com/fourback/redisstudy/domain/user/service/UserService.java
@@ -6,7 +6,7 @@ import com.fourback.redisstudy.domain.user.dto.response.UserCreateResponseDto;
 import com.fourback.redisstudy.domain.user.dto.response.UserInquiryResponseDto;
 import com.fourback.redisstudy.domain.user.dto.response.UserLoginResponseDto;
 import com.fourback.redisstudy.global.common.enums.PrefixEnum;
-import com.fourback.redisstudy.global.common.repository.RedisRepository;
+import com.fourback.redisstudy.global.common.repository.CommonRepository;
 import com.fourback.redisstudy.global.common.util.EncryptionUtil;
 import com.fourback.redisstudy.global.common.util.RandomUtil;
 import lombok.RequiredArgsConstructor;
@@ -17,42 +17,42 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final RedisRepository redisRepository;
+    private final CommonRepository commonRepository;
 
     public UserCreateResponseDto create(UserCreateRequestDto createRequestDto) {
         String userId = RandomUtil.genId();
 
         String username = createRequestDto.getUsername();
 
-        if (redisRepository.sIsMember(PrefixEnum.UNIQUE_USER.getPrefix(), username)) {
+        if (commonRepository.sIsMember(PrefixEnum.UNIQUE_USER.getPrefix(), username)) {
             throw new RuntimeException("중복된 아이디");
         }
 
         String encryptPassword = EncryptionUtil.encryptPassword(createRequestDto.getPassword());
         createRequestDto.setPassword(encryptPassword);
 
-        redisRepository.hSet(PrefixEnum.USER.getPrefix() + userId, createRequestDto.toMap());
-        redisRepository.sAdd(PrefixEnum.UNIQUE_USER.getPrefix(), username);
-        redisRepository.zAdd(PrefixEnum.USERNAME.getPrefix(), username, Integer.parseInt(userId, 16));
+        commonRepository.hSet(PrefixEnum.USER.getPrefix() + userId, createRequestDto.toMap());
+        commonRepository.sAdd(PrefixEnum.UNIQUE_USER.getPrefix(), username);
+        commonRepository.zAdd(PrefixEnum.USERNAME.getPrefix(), username, Integer.parseInt(userId, 16));
 
         return UserCreateResponseDto.from(userId);
     }
 
     public UserInquiryResponseDto get(String userId) {
-        Map<String, String> inquiryMap = redisRepository.hGetAll(PrefixEnum.USER.getPrefix() + userId);
+        Map<String, String> inquiryMap = commonRepository.hGetAll(PrefixEnum.USER.getPrefix() + userId);
 
         return UserInquiryResponseDto.from(inquiryMap);
     }
 
     public UserLoginResponseDto login(UserLoginRequestDto loginRequestDto) {
-        Double decimalId = redisRepository.zScore(PrefixEnum.USERNAME.getPrefix(), loginRequestDto.getUsername());
+        Double decimalId = commonRepository.zScore(PrefixEnum.USERNAME.getPrefix(), loginRequestDto.getUsername());
         if (decimalId == null) {
             throw new RuntimeException("존재하지 않은 아이디");
         }
 
         String userId = Integer.toHexString(decimalId.intValue());
 
-        Map<String, String> userInquiryMap = redisRepository.hGetAll(PrefixEnum.USER.getPrefix() + userId);
+        Map<String, String> userInquiryMap = commonRepository.hGetAll(PrefixEnum.USER.getPrefix() + userId);
         String hashedPassword = userInquiryMap.get("password");
 
         if (!EncryptionUtil.checkPassword(loginRequestDto.getPassword(), hashedPassword)) {
